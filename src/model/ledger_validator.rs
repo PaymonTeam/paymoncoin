1ca5c861c5811dcc261445f86d983257ac2de60e
csguy88 <37470384+gdsoftwares8@users.noreply.github.com>
2018-05-20 12:35:10 +0530

    added pending functions

--- src/model/ledger_validator.rs
+++ src/model/ledger_validator.rs
@@ -122,10 +122,22 @@ impl LedgerValidator {
         Ok(())
     }
 
-    pub fn init(&mut self) {
-    // TODO
-    }
-
+pub fn init(&mut self)
+    {
+    
+    let latestConsistentMilestone :MilestoneViewModel ;
+     latestConsistentMilestone  =buildSnapshot();
+ 
+    if(latestConsistentMilestone!=null )
+       {
+ 
+       debug!("Loaded consistent milestone:= {}",latestConsistentMilestone.index());
+       milestone.latestSolidSubtangleMilestone = latestConsistentMilestone.getHash();
+       milestone.latestSolidSubtangleMilestoneIndex = latestConsistentMilestone.index();
+   
+      }
+    
+}
     pub fn update_snapshot(&mut self, milestone: &Milestone) -> Result<bool, TransactionError> {
         // TODO
 //        let mut transaction;
@@ -155,23 +167,58 @@ impl LedgerValidator {
 //
 //    }
 
-    pub fn update_diff(&mut self, approved_hashes: HashSet<Hash>, diff: HashMap<Address, i64>, tip:
+    
+pub fn update_diff(&mut self, approved_hashes: HashSet<Hash>, diff: HashMap<Address, i64>, tip:
     Hash) -> Result<bool, TransactionError> {
-        // TODO
-//        if let Ok(mut hive) = self.hive.lock() {
-//            match hive.storage_load_transaction(&na_hash) {
-//                Some(t) => {
-//                    if t.is_solid() {
-//                        return false;
-//                    }
-//                },
-//                None => return Err(TransactionError::InvalidHash)
-//            };
-//        }
-
-        let is_consistent = false; // TODO
+ 
+         if let Ok(mut hive) = self.hive.lock() {
+             match hive.storage_load_transaction(&na_hash) {
+                 Some(t) => {
+                     if t.is_solid() {
+                         return false;
+                     }
+                 },
+                 None => return Err(TransactionError::InvalidHash)
+             };
+         }
+        let mut visited_hashes = HashSet::<Hash>::new(approved_hashes);
+		let mut currentState = getLatestDiff (visited_hashes,tip,milestone.latestSnapshot.index(),false);
+		if(currentState==null )
+		{
+		retutn false;
+		}
+		
+			for(key,value) in &*diff
+		{
+		   if (currentState.insert(key,((hash, aLong) -> value + aLong)) == null)
+		       {
+			      currentState.insert(key, value);
+			   }
+		}
+		
+        let is_consistent = snapshot.isConsistent(milestone.latestSnapshot.patchedDiff(currentState));
         if is_consistent {
+		
+		    diff.insert(currentState);
+            approvedHashes.insert(visitedHashes);
         }
         Ok(is_consistent)
     }
+    
+pub fn checkConsistency(hash: List (Hash))
+{
+   let mut visited_hashes = HashSet::<Hash>::new();
+   let mut diff = HashMap::<hash, f64>::new();
+   
+   
+   for hash_param in hash.iter()
+   {
+    
+	if(!update_diff(visited_hashes, diff, hash_param))
+	  {
+	     return false; 
+	  }
+   
+   }
+   return true;
 }
